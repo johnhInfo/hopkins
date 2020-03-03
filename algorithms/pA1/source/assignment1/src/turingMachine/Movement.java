ab366888b06a19bf3c1bbef8afcab7219b1724da
Herrmann, John <john_herrmann@baxter.com>
2020-03-03 17:47:45 -0600

    Updated comments to Movement.java and MovementTest.java to improve readibility

--- algorithms/pA1/source/assignment1/src/turingMachine/Movement.java
+++ algorithms/pA1/source/assignment1/src/turingMachine/Movement.java
@@ -1,13 +1,20 @@
 package turingMachine;
+/******************************************************************************
+ * File: Movement.java
+ * Assignment: PA1
+ * Author: John A. Herrmann
+ * Class: EN.605.621.82.SP20 Foundations of Algorithms
+ * Date: 3/2/2020
+ *******************************************************************************/
 
-/**
- * Specifies the next movement for the Turing machine's head 
+/*******************************************************************************
+ * Movement Object specifies the next movement for the Turing machine's head 
  *
  * RIGHT move the head to the right 
  * LEFT  move the heat to the left 
  * HALT_PASS halt the head and return "PASS"
  * HALT_FAIL halt the head and return "FAIL"
- */
+ ******************************************************************************/
 public enum Movement {
 	RIGHT,
 	LEFT,
--- algorithms/pA1/source/assignment1/src/turingMachine/MovementTest.java
+++ algorithms/pA1/source/assignment1/src/turingMachine/MovementTest.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * File: MovementTest.java
+ * Assignment: PA1
+ * Author: John A. Herrmann
+ * Class: EN.605.621.82.SP20 Foundations of Algorithms
+ * Date: 3/2/2020
+ *******************************************************************************/
 package turingMachine;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,36 +12,36 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 
-/**
+/**************************************************************************
  * Tests the Movement Enum Class
  *
  * RIGHT move the head to the right 
  * LEFT  move the heat to the left 
  * HALT_PASS halt the head and return "PASS"
  * HALT_FAIL halt the head and return "FAIL"
- */
+ *************************************************************************/
 class MovementTest {
 
 	@Test
-	void testMovementRight() {
+	void testCase_MovementRight() {
 		Movement next = Movement.RIGHT;
 		assertEquals(Movement.RIGHT, next, "*Right* assignment failed");
 	}
 	
 	@Test
-	void testMovementLEFT() {
+	void testCase_MovementLEFT() {
 		Movement next = Movement.LEFT;
 		assertEquals(Movement.LEFT, next, "*Left* assignment failed");
 	}
 	
 	@Test
-	void testMovementHALT_PASS() {
+	void testCase_MovementHALT_PASS() {
 		Movement next = Movement.HALT_PASS;
 		assertEquals(Movement.HALT_PASS, next, "*HALT_PASS* assignment fails");
 	}
 	
 	@Test
-	void testMovementHALT_FAIL() {
+	void testCase_MovementHALT_FAIL() {
 		Movement next = Movement.HALT_FAIL;
 		assertEquals(Movement.HALT_FAIL, next, "*HALT_FAIL* assignment fails");
 	}
