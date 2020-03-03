a18bde3cfa339352b90b0cc35568db30e5c204bf
Herrmann, John <john_herrmann@baxter.com>
2020-03-03 17:49:38 -0600

    Updated comments to MultiplicationDTM.java and MultiplicationDTMTest.java to improve readibility

--- algorithms/pA1/source/assignment1/src/turingMachine/MultiplicationDTMTest.java
+++ algorithms/pA1/source/assignment1/src/turingMachine/MultiplicationDTMTest.java
@@ -1,3 +1,10 @@
+/******************************************************************************
+ * File: MultiplicationDTMTest.java
+ * Assignment: PA1
+ * Author: John A. Herrmann
+ * Class: EN.605.621.82.SP20 Foundations of Algorithms
+ * Date: 3/2/2020
+ *******************************************************************************/
 package turingMachine;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
