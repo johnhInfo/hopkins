8a44b65527b3d64eb7c84745750e22b2152be9b7
Herrmann, John <john_herrmann@baxter.com>
2020-03-03 17:36:09 -0600

    Updated comments to ActionTest.java to improve readibility

--- algorithms/pA1/source/assignment1/src/turingMachine/ActionTest.java
+++ algorithms/pA1/source/assignment1/src/turingMachine/ActionTest.java
@@ -1,17 +1,24 @@
+/******************************************************************************
+ * File: ActionTest.java
+ * Assignment: PA1
+ * Author: John A. Herrmann
+ * Class: EN.605.621.82.SP20 Foundations of Algorithms
+ * Date: 3/2/2020
+ *******************************************************************************/
 package turingMachine;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * The following class tests the Action class. 
+/*******************************************************************************
+ * ActionTest contains unit tests for the Action class 
  *
- */
+ ******************************************************************************/
 class ActionTest {
 
 	@Test
-	void testAction() {
+	void testCase1() {
 		Action NewAction =  new Action('1', Movement.LEFT, 3);
 		
 		assertEquals('1', NewAction.writeSymbol, "*1* assignment failed");
