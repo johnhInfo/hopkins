7f8f280a19b7aeece6592f4789a0adcb0da54122
Herrmann, John <john_herrmann@baxter.com>
2020-03-03 15:55:08 -0600

    Commit of placeholder code and placeholder test for multiplicationDTM

--- algorithms/pA1/source/assignment1/src/turingMachine/multiplicationDTM.java
+++ algorithms/pA1/source/assignment1/src/turingMachine/multiplicationDTM.java
@@ -1,7 +1,155 @@
 package turingMachine;
-
-/* https://www.youtube.com/watch?v=3mrMqNe0MUY */
-
+/******************************************************************************
+ * multiplicationDTM 
+ * 
+ * references: https://www.youtube.com/watch?v=X7bxtpNktAE
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author johnherrmann
+ * @date 3/2/2020
+ ******************************************************************************/
 public class multiplicationDTM {
+	
+	public char[] tape;				/*The tape containing input params 		 */
+	
+	/*************************************************************************
+	 * Constructor Creates an object of the additionDTM class and initializes
+	 *             ... the input tape
+	 *             
+	 * @param inputTape a char[] modeling an input tape 
+	 *************************************************************************/
+	public multiplicationDTM(char[] inputTape) {
+		this.tape = inputTape;		
+	}
+	/*************************************************************************
+	 * program The program method takes the inputTape stored during object
+	 *         ...initialization and attempts to execute the DTM
+	 * 
+	 * @return char[] the output tape 
+	 ************************************************************************/
+	public char[] program()
+	{
+		/********************************************************************
+		 * Control Flow:
+		 * 1. Create states Q0-Q3
+		 * 2. Create the state[]
+		 * 3. Initialize the DTM 
+		 *******************************************************************/
+		
+		/********************************************************************
+		 * Creation of state Q0
+		 ********************************************************************/
+										/* Input = 1 		THEN: 
+										 *   - Write: 'b'		SIGNALS START
+										 *   - Move: Right
+										 *   - Next State: 1				*/
+		action q0_0_action = new action('b',Movement.RIGHT, 1);
+		transitionFunction tf_q0_0 = new transitionFunction('1', q0_0_action);
+		
+										/* Input = b 		THEN: 
+										 *   - Write: 'f'		
+										 *   - Move: HALT_FAIL
+										 *   - Next State: 0				*/
+		action q0_b_action = new action('f',Movement.HALT_FAIL, 0);
+		transitionFunction tf_q0_b = new transitionFunction('b', q0_b_action);
+		
+										/*Create an array of transition functions 
+										 *... for state Q0 					*/
+		transitionFunction[] q0TFs = {tf_q0_0, tf_q0_b};
+		state q0 = new state(0, q0TFs); /* Create state Q0					*/
+		
+		/********************************************************************
+		 * Creation of state Q1
+		 ********************************************************************/
+										/* Input = 0 THEN: 
+										 *   - Write: Zero
+										 *   - Move: Right
+										 *   - Next State: 1				*/
+		action q1_0_action = new action('0',Movement.RIGHT, 1);
+		transitionFunction tf_q1_0 = new transitionFunction('0', q1_0_action);
+		
+										/* Input = 1 THEN: 
+										 *   - Write: 1
+										 *   - Move: Right
+										 *   - Next State: 1				*/
+		action q1_1_action = new action('1',Movement.RIGHT, 1);
+		transitionFunction tf_q1_1 = new transitionFunction('1', q1_1_action);
 
+										/* Input = b THEN: 
+										 *   - Write: b
+										 *   - Move: Left
+										 *   - Next State: 2				*/
+		action q1_b_action = new action('b',Movement.LEFT, 2);
+		transitionFunction tf_q1_b = new transitionFunction('b', q1_b_action);
+										/*Create an array of transition functions 
+										 *... for state Q1 					*/
+		transitionFunction[] q1TFs = {tf_q1_0, tf_q1_1, tf_q1_b};
+		state q1 = new state(1, q1TFs);/* Create state Q1					*/
+		
+		/********************************************************************
+		 * Creation of state Q2
+		 ********************************************************************/
+										/* Input = 0 THEN: 
+										 *   - Write: 1
+										 *   - Move: Left
+										 *   - Next State: HALT_PASS		*/
+		action q2_0_action = new action('1',Movement.HALT_PASS, 2);
+		transitionFunction tf_q2_0 = new transitionFunction('0', q2_0_action);
+		
+										/* Input = 1 THEN: 
+										 *   - Write: b
+										 *   - Move: Left
+										 *   - Next State: 3				*/
+		action q2_1_action = new action('b',Movement.LEFT, 3);
+		transitionFunction tf_q2_1= new transitionFunction('1', q2_1_action);
+		
+		transitionFunction[] q2TFs = {tf_q2_1, tf_q2_0};
+		state q2 = new state(2, q2TFs);
+		
+		/********************************************************************
+		 * Creation of state Q3
+		 ********************************************************************/
+										/* Input = 0 THEN: 
+										 *   - Write: 0
+										 *   - Move: Left
+										 *   - Next State: 3				*/
+		action q3_0_action = new action('0',Movement.LEFT, 3);
+		transitionFunction tf_q3_0 = new transitionFunction('0', q3_0_action);
+		
+										/* Input = 0 THEN: 
+										 *   - Write: 0
+										 *   - Move: Left
+										 *   - Next State: 3				*/
+		action q3_1_action = new action('1',Movement.LEFT, 3);
+		transitionFunction tf_q3_1 = new transitionFunction('1', q3_1_action);
+		
+										/* Input = b THEN: 
+										 *   - Write: b
+										 *   - Move: right
+										 *   - Next State: 0				*/
+		action q3_b_action = new action('b',Movement.RIGHT, 0);
+		transitionFunction tf_q3_b = new transitionFunction('b', q3_b_action);
+		
+		transitionFunction[] q3TFs = {tf_q3_0, tf_q3_1, tf_q3_b};
+		state q3 = new state(3, q3TFs);
+		
+		/********************************************************************
+		 * Initialize the DTM 
+		 ********************************************************************/
+		 char[] alphabet = {'0','1','f'};
+		 char blankSymbol = 'b';
+		 state[] states = {q0, q1, q2, q3};
+		 int initialState = 0;
+		 genericDTM multiplicationDTM = new genericDTM(alphabet, blankSymbol,
+				                     this.tape, states, initialState);
+		 /********************************************************************
+		  * Execute the DTM
+		  ********************************************************************/
+		 multiplicationDTM.program();
+		 
+		 return multiplicationDTM.tape.tape; 
+	}
 }
---
+++ algorithms/pA1/source/assignment1/src/turingMachine/multiplicationDTMTest.java
@@ -0,0 +1,62 @@
+package turingMachine;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+
+class multiplicationDTMTest {
+	
+	/***********************************************************
+	 * Test: 3-2
+	 ***********************************************************/
+	@Test
+	void test1() {
+		
+		 char[] tape = {'1', '1','1','0','1','1','b'};
+		 char[] success = {'b', 'b','b','1','b','b','b'};
+		 multiplicationDTM test1 = new multiplicationDTM(tape);
+		 char[] output = test1.program();
+		 boolean equals = Arrays.equals(output, success);
+		 assertEquals(equals, true, "3-2 Calculation Failed");
+	}
+	
+	/***********************************************************
+	 * Test: 3-3
+	 ***********************************************************/
+	@Test
+	void test2() {
+		
+		 char[] tape = {'1', '1','1','0','1','1','1','b'};
+		 char[] success = {'b', 'b','b','0','b','b','b','b'};
+		 multiplicationDTM test1 = new multiplicationDTM(tape);
+		 char[] output = test1.program();
+		 boolean equals = Arrays.equals(output, success);
+		 assertEquals(equals, true, "3-3 Calculation Failed");
+	}
+	/***********************************************************
+	 * Test: 7-2
+	 ***********************************************************/
+	@Test
+	void test3() {
+		
+		char[] tape = {'1', '1','1','1','1','1','1','0','1','1','b'};
+		char[] success = {'b', 'b','b','1','1','1','1','1','b','b','b'};
+		multiplicationDTM test1 = new multiplicationDTM(tape);
+		char[] output = test1.program();
+		boolean equals = Arrays.equals(output, success);
+		assertEquals(equals, true, "7-2 Calculation Failed");
+	}
+	/***********************************************************
+	 * Test: 9-2
+	 ***********************************************************/
+	@Test
+	void test4() {
+		char[] tape = {'1', '1','1','1','1','1','1','1','1','0','1','1','b'};
+		char[] success = {'b','b','b','1','1','1','1','1','1','1','b','b','b'};
+		multiplicationDTM test1 = new multiplicationDTM(tape);
+		char[] output = test1.program();
+		System.out.println(output);
+		boolean equals = Arrays.equals(output, success);
+		assertEquals(equals, true, "9-2 Calculation Failed");	
+	}
+}
